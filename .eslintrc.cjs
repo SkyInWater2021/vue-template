11a912c678992a3b177ab52c15c7f586448f9c6e
heyuan <1024230520@qq.com>
2023-10-17 17:16:14 +0800

    chore: initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+
+  globals: {},
+
+  parser: 'vue-eslint-parser', // 用于解析.vue文件
+
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+
+  plugins: ['vue', '@typescript-eslint'],
+
+  rules: {},
+}
